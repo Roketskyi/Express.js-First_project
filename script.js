d2143adb860f6e5ca9a62f473561c7e8ce21ef48
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-04-05 22:01:18 +0300

    update

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const { MongoClient } = require('mongodb');
 
 const app = express();
 
+const IP = 'localhost';
+const PORT = 3000;
+
 const url = "mongodb+srv://romanroketskiy05:Roman080805MLP@reynes.73bphty.mongodb.net/?retryWrites=true&w=majority";
 const dbName = 'myProject';
 const client = new MongoClient(url);
@@ -17,20 +20,31 @@ connectToDb().catch(console.error);
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.send('Hello world!');
 })
 
-app.post('/temperature', async (req, res) => {
-  const data = req.body;
+app.post('/addArray', async (req, res) => {
+  const data = [req.body];
   const db = client.db(dbName);
   const collection = db.collection('temperatureData');
   
   try {
-    const result = await collection.insertMany(data);
+    const maxId = await collection.find().sort({ id: -1 }).limit(1).toArray();
+    const newId = maxId.length > 0 ? maxId[0].id + 1 : 1;
 
-    console.log(`${result.insertedCount} documents were inserted`);
-    res.status(201).send.json('good');
+    const dataWithDate = data.map((item) => ({ 
+      serialNumber: item.serialNumber,
+      temperature: item.temperature,
+      date: new Date().toISOString().replace('T', '-').replace(/\..*/, '') 
+    }));
 
+    const dataWithId = dataWithDate.map((item) => ({ id: newId, ...item }));
+  
+    const result = await collection.insertMany(dataWithId);
+
+    console.log(`${result.insertedCount} information has been added to the database`);
+
+    res.status(201).send('information has been added to the database');
   } catch (err) {
     console.error(err);
     
@@ -38,33 +52,51 @@ app.post('/temperature', async (req, res) => {
   }
 });
 
-app.get('/base', async (req, res) => {
-    const db = client.db(dbName);
-    const collection = db.collection('temperatureData');
+app.get('/base/:id?', async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection('temperatureData');
 
-    try {
-        const documents = await collection.find().toArray();
-        res.json(documents);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send(err.message);
-    }
+  try {
+      if (req.params.id) {
+          const document = await collection.findOne({ id: +req.params.id });
+          if (document) {
+              res.json(document);
+          } else {
+              res.status(404).send('There is no information');
+          }
+      } else {
+          const documents = await collection.find().toArray();
+          res.status(201).json(documents);
+      }
+  } catch (err) {
+      console.error(err);
+      res.status(500).send(err.message);
+  }
 });
 
-app.get('/base/:id', async (req, res) => {
-    const db = client.db(dbName);
-    const collection = db.collection('temperatureData');
-    console.log(req.params.id)
-    try {
-        const document = await collection.find({ id: +req.params.id}).toArray();
-        res.json(document);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send(err.message);
+app.delete('/cleanArray/:id?', async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection('temperatureData');
+
+  try {
+    if (req.params.id) {
+      const result = await collection.deleteOne({ id: +req.params.id });
+      console.log(`Deleted document with id ${req.params.id}`);
+      
+      res.json(result);
+    } else {
+      const result = await collection.drop();
+      console.log(`Dropped collection ${collection.collectionName}`);
+
+      res.json(result);
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
 });
 
 
 app.listen(3000, () => {
-  console.log('Server listening on port http://localhost:3000/');
+  console.log(`Server listening on http://${IP}:${PORT}/`);
 });
